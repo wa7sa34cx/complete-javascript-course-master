490c3fdab9fc46e3499f57289eaa568dfeb69d15
Alex Black <wa7@sa34.cx>
2021-03-03 13:35:56 +0300

    Challenge 3

---
+++ 09-Data-Structures-Operators/challenge-3.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+console.log(gameEvents);
+
+// 1. Create an array 'events'of the different game events that happened (no duplicates)
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3.
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+// 4.
+for (const [time, event] of gameEvents.entries()) {
+  // console.log(time, event);
+  const half = time < 45 ? '[FIRST HALF]' : '[SECOND HALF]';
+  console.log(half, time, event);
+}
