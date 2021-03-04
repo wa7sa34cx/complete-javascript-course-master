c7fbcff0906033dc1f839bf8766d8aeb21f792a0
Alex Black <wa7@sa34.cx>
2021-03-04 23:54:41 +0300

    Thursday

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(fn(str));
+  console.log(`Transformed by ${fn.name}`);
+};
+
+transformer('JavaScript is awesome!', upperFirstWord);
+transformer('JavaScript is awesome!', oneWord);
