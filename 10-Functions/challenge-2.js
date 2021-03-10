6341f45adfb5c22d06e2633bcced8523a2ffbbeb
Alex Black <wa7@sa34.cx>
2021-03-10 18:50:10 +0300

    Challenge 2

---
+++ 10-Functions/challenge-2.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Challenge - 2
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
